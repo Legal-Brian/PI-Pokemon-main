e6deaa2ca1fa15ede381846ae8fc0178671dbe74
Legal-Brian <legal.b.n@outlook.com>
2022-12-06 20:01:30 -0300

    models added

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -7,6 +7,36 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
-  });
+    image: {
+      type: DataTypes.STRING,
+    },
+    hp: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+    },
+  },
+  { timestamps: false }
+);
 };
