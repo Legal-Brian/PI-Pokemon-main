c891e8de383af1eeeb682a09746972be66072382
Legal-Brian <legal.b.n@outlook.com>
2022-12-07 19:21:59 -0300

    type model edited

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -3,12 +3,6 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo para los Tipos de Pokemon
   sequelize.define("type", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
