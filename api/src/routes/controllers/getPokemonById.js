8a50795a8b20d6f5266d19fef4ba9216dd8caa9a
Legal-Brian <legal.b.n@outlook.com>
2022-12-08 22:01:15 -0300

    corregido

--- api/src/routes/controllers/getPokemonById.js
+++ api/src/routes/controllers/getPokemonById.js
@@ -1,43 +1,45 @@
 const fetch = require("node-fetch")
-const { Pokemon } = require("../../db");
+const { Pokemon, Type} = require("../../db");
 
 const getPokemonById = async (id) => {
     try {
-        if (isNaN(id)) {
-            const db = await Pokemon.findOne({where: { id: id }});
-            const pokemonDb = [{
-                id: db.id,
-                name: db.name,
-                type: db.type.map((t) => t.name),
-                image: db.image,
-                hp: db.hp,
-                attack: db.attack,
-                defense: db.defense,
-                speed: db.speed,
-                height: db.height,
-                weight: db.weight,
-            }];
-            return pokemonDb;
-        } else {
-            const api = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-            const info = await api.json();
-            const pokemonId = [{
-                id: info.id,
-                name: info.name,
-                type: info.types.map((t) => t.type.name),
-                image: info.sprites.other["official-artwork"].front_default,
-                hp: info.stats[0].base_stat,
-                attack: info.stats[1].base_stat,
-                defense: info.stats[2].base_stat,
-                speed: info.stats[5].base_stat,
-                height: info.height,
-                weight: info.weight,
-            }];
-            return pokemonId;
-        }
-    } catch (error) {
-        throw new Error("Pokemon not found")   
-    }
+        const api = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await api.json();
+    
+        const pokemonId = [{
+          id: data.id,
+          name: data.name,
+          image: data.sprites.other["official-artwork"].front_default,
+          hp: data.stats[0].base_stat,
+          attack: data.stats[1].base_stat,
+          defense: data.stats[2].base_stat,
+          speed: data.stats[5].base_stat,
+          height: data.height,
+          weight: data.weight,
+          types: data.types.map((t) => t.type.name),
+        }];
+    
+        return pokemonId;
+      } catch (error) {}
+      try {
+        const db = await Pokemon.findByPk(id, { include: Type });
+        const pokemonDb = [{
+          id: db.id,
+          name: db.name,
+          image: db.image,
+          hp: db.hp,
+          attack: db.attack,
+          defense: db.defense,
+          speed: db.speed,
+          height: db.height,
+          weight: db.weight,
+          types: db.types.map((t) => t.type.name),
+        }];
+    
+        return pokemonDb;
+      } catch (error) {
+        return {};
+      }
 };
 
 module.exports= getPokemonById;
