e32337c51dc5c7c876d2fdca31ff63dfcda2bded
Legal-Brian <legal.b.n@outlook.com>
2022-12-09 13:27:00 -0300

    getTypes updated

--- api/src/routes/controllers/getTypes.js
+++ api/src/routes/controllers/getTypes.js
@@ -5,11 +5,11 @@ const getTypes = async () => {
 const api = await fetch('https://pokeapi.co/api/v2/type');
 const types = await api.json();
 for( let type of types.results ) {
-    Type.findOrCreate({
+    await Type.findOrCreate({
         where: {name: type.name}
     })
 }
-return Type.findAll();
+return await Type.findAll();
 }
 
 module.exports = getTypes
