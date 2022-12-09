57ddb1111d5b034820f21a123fc0376898bbda2f
Legal-Brian <legal.b.n@outlook.com>
2022-12-09 13:05:11 -0300

    getApiPokemon updated

--- api/src/routes/controllers/getAllPokemons.js
+++ api/src/routes/controllers/getAllPokemons.js
@@ -2,26 +2,45 @@ const { Pokemon, Type } = require("../../db");
 const fetch = require("node-fetch")
 
 const getApiPokemon = async () => {
-  const api = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-  const data = await api.json();
-  const pokemonInfo = [];
-  for (i = 0; i < data.results.length; i++) {
-      const pokemon = await fetch(data.results[i].url);
-      const info = await pokemon.json();
-      pokemonInfo.push({
-        id: info.id,
-        name: info.name,
-        image: info.sprites.other["official-artwork"].front_default,
-        hp: info.stats[0].base_stat,
-        attack: info.stats[1].base_stat,
-        defense: info.stats[2].base_stat,
-        speed: info.stats[5].base_stat,
-        height: info.height,
-        weight: info.weight,
-        types: info.types.map((t) => t.type.name),
-      });
+  const search = async(url) => {
+    let aux = {pokemon: null, next: ""};
+    await fetch(url)
+    .then((res)=> res.json())
+    .then((data)=> {
+      return {urls: data.results.map((x) => x.url), next: data.next};
+    })
+    .then(async (data) => {
+      await Promise.all(
+        data.urls.map((x) => fetch(x).then((res)=> res.json()).catch(err =>{throw new Error(err.message)}))
+      ).then((data)=>{
+        aux.pokemon = data.map((x)=>{
+          return {
+            id: x.id,
+            name: x.name,
+            image: x.sprites.other["official-artwork"].front_default,
+            hp: x.stats[0].base_stat,
+            attack: x.stats[1].base_stat,
+            defense: x.stats[2].base_stat,
+            speed: x.stats[5].base_stat,
+            height: x.height,
+            weight: x.weight,
+            types: x.types.map((t) => t.type.name),
+          };
+        });
+      }).catch(err => {
+        throw new Error(err.message);
+    })
+    aux.next = data.next
+    })
+    .catch((err)=>{
+      throw new Error(err.message);
+    })
+    return aux
   }
-  return pokemonInfo;
+  const firstTwenty = await search("https://pokeapi.co/api/v2/pokemon");
+  const secondTwenty = await search(firstTwenty.next);
+  const apiConcat = await firstTwenty.pokemon.concat(secondTwenty.pokemon);
+  return apiConcat;
 };
 
 const getDbPokemon = async () => {
