cf10ddcfa6c542b3c61bc9cc42dfa01d38ed43aa
Legal-Brian <legal.b.n@outlook.com>
2022-12-09 15:16:43 -0300

    validations added

--- api/src/routes/controllers/postPokemon.js
+++ api/src/routes/controllers/postPokemon.js
@@ -2,33 +2,44 @@ const { Pokemon , Type } = require("../../db");
 const getTypes = require("./getTypes")
 
 const postPokemon = async (name, hp, attack, defense, speed, height, weight, types , image,) => {
-    if ( 
-        isNaN(hp) || isNaN(attack) || isNaN(defense) || isNaN(speed) || isNaN(height) || isNaN(weight)
-    ) throw new Error("Alguno de los argumentos no es un numero");
-  
-    if (!name) throw new Error("El nombre es obligatorio" );
-  
-    const existe = await Pokemon.findOne({ where: { name: name } });
-    if (existe) throw new Error("El pokemon ya existe" );
-    else{
-        const pokemon = await Pokemon.create({
-            name: name.toLowerCase(),
-            image,
-            hp,
-            attack,
-            defense,
-            speed,
-            height,
-            weight,
-        });
-        const allTypes = await getTypes();
-        await types.forEach(async el =>{
-            await allTypes 
-            await pokemon.addType(await Type.findAll({
-                where: {name: el.name}
-            }));
-        });
-        return "Pokemon creado con exito";
+    try{
+        if (!name) {
+            throw new Error("Name is required" );
+        }
+        
+        if (isNaN(hp) || isNaN(attack) || isNaN(defense) || isNaN(speed) || isNaN(height) || isNaN(weight)) throw new Error("Incorrect data type");
+        
+        else{
+            if (!image) image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/800px-Pokebola-pokeball-png-0.png"
+            if (!types ) types = [{"name": "normal"}]
+            if (types.length > 2) throw new Error("Can have no more than two types")
+            
+            const exist = await Pokemon.findOne({ where: { name: name } });
+            if (exist) throw new Error("Pokemon already exist");
+            
+            else {
+                const pokemon = await Pokemon.create({
+                    name: name.toLowerCase(),
+                    image,
+                    hp,
+                    attack,
+                    defense,
+                    speed,
+                    height,
+                    weight,
+                });
+                await getTypes();
+                await types.forEach(async el =>{
+                    await pokemon.addType(await Type.findAll({
+                        where: {name: el.name}
+                    }));
+                });
+            }
+            return "Pokemon successfully created";
+        }
+    }
+    catch (error){
+        return(error.message)
     }
 }
 
