69ce44cc27b9d39e83a7f93655c273ca6e8889c8
Legal-Brian <legal.b.n@outlook.com>
2022-12-09 01:31:57 -0300

    getTypes controller added

--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -1,19 +1,13 @@
 const { Router } = require("express");
 const fetch = require("node-fetch")
-const {Type} = require("../db")
+const getTypes = require("./controllers/getTypes")
+
 
 const router = Router();
 
 router.get('/', async (req, res) => {
     try {
-        const api = await fetch('https://pokeapi.co/api/v2/type');
-        const types = await api.json();
-        for( let type of types.results ) {
-            const exist = await Type.findOne({where: { name: type.name }})
-            if(exist) return res.json(await Type.findAll())
-            await Type.create({ name: type.name})
-        }
-        res.json(await Type.findAll());
+        res.json(await getTypes());
     } catch (error) {
         res.status(404).json(error.message)   
     }
