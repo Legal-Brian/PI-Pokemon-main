ba0ff7b63615d8dad715273487d1c26a6b8251a4
Legal-Brian <legal.b.n@outlook.com>
2022-12-14 13:14:55 -0300

    store added

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
+
+// envolvi el archivo raiz en un provider para poder pasarle el store.
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
