3c611fa90b3ebcddb88cf64f419ef2a2d186eba7
Legal-Brian <legal.b.n@outlook.com>
2022-12-14 20:13:09 -0300

    default case added

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,4 +1,18 @@
-const rootReducer = () => {
 
+const inicialState = {
+    pokemons : []
+}
+
+const rootReducer = (state = inicialState, action) => {
+    switch(action.type){
+        case "GET_POKEMONS":
+            return{
+                ...state,
+                pokemons: action.payload,
+            }
+            default:
+                return state;
+    }
 };
+
 export default rootReducer;
