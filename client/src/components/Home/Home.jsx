48d928c3d68ded8a9d85b3cd60acc7c67db906c9
Legal-Brian <legal.b.n@outlook.com>
2022-12-16 20:01:47 -0300

    orderByName & orderByAttack added

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getPokemons, filterPokemonsByType, getTypes } from "../../actions/index";
+import { getPokemons, filterPokemonsByType, getTypes, filterCreated, orderByName, orderByAttack} from "../../actions/index";
 import Card from "../Card/Card";
 import Paginated from "../Paginated/Paginated";
 
@@ -10,6 +10,7 @@ const Home = () => {
     const dispatch = useDispatch();
     const allPokemons = useSelector((state) => state.pokemons);
     const allTypes = useSelector((state) => state.types)
+
 
     useEffect(()=>{
         dispatch(getPokemons());
@@ -21,6 +22,8 @@ const Home = () => {
     const indexOfLastPokemon = currentPage * pokemonsPerPage;
     const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
     const currentPokemons = allPokemons.slice(indexOfFirstPokemon, indexOfLastPokemon)
+    const [ordenByName, setOrdenByName] = useState("");
+    const [ordenByAttack, setOrdenByAttack] = useState("");
 
     const paginated = (pageNumber) => {
         setCurrentPage(pageNumber);
@@ -32,18 +35,40 @@ const Home = () => {
     }
 
     const handleFilterType = (event) => {
+        event.preventDefault()
         dispatch(filterPokemonsByType(event.target.value));
     }
 
+    const handleFilterCreated = (event) =>{
+        event.preventDefault()
+        dispatch(filterCreated(event.target.value));
+    }
+    
+    const handleOrderedByName = (event) => {
+        event.preventDefault();
+        dispatch(orderByName(event.target.value));
+        setCurrentPage(1);
+        setOrdenByName(`Ordenado ${event.target.value}`)
+    }
+    const handleOrderedByAttack = (event) => {
+        event.preventDefault();
+        dispatch(orderByAttack(event.target.value));
+        setCurrentPage(1);
+        setOrdenByAttack(`Ordenado ${event.target.value}`)
+    }
     return(
         <div>
             <Link to="/pokemon">Create Pokemon</Link>
             <h1>Pokedex</h1>
-            <button onClick={e=>{handlerClick(e)}}>Recharge Pokemons</button>
             <div>
-                <select>
+                <button onClick={e=>{handlerClick(e)}}>Restore</button>
+                <select onChange={e => handleOrderedByName(e)}>
                     <option value="ascendant">Ascendant</option>
                     <option value="descendant">Descendant</option>
+                </select>
+                <select onChange={e => handleOrderedByAttack(e)}>
+                    <option value="biggest attack">Biggest Attack</option>
+                    <option value="minor attack">Minor Attack</option>
                 </select>
                 <select onChange={e => handleFilterType(e)}>
                     <option value="all">All</option>
@@ -51,13 +76,13 @@ const Home = () => {
                     <option value={ele.name}>{ele.name[0].toUpperCase()+ele.name.slice(1)}</option>
                     ))}
                 </select>
-                <select>
-                <option value="all">All</option>
-                <option value="created">Created</option>
-                <option value="existing">Existing</option>
+                <select onChange={e => handleFilterCreated(e)}>
+                    <option value="all">All</option>
+                    <option value="created">Created</option>
+                    <option value="existing">Existing</option>
                 </select>
                 <Paginated pokemonsPerPage={pokemonsPerPage} allPokemons={allPokemons.length} paginated={paginated}/>
-                {
+                    {
                     currentPokemons?.map(ele => {
                         return(
                             <fragment className="container" >
@@ -67,7 +92,7 @@ const Home = () => {
                             </fragment>
                         )
                     })
-                }
+                    }
                 <Paginated pokemonsPerPage={pokemonsPerPage} allPokemons={allPokemons.length} paginated={paginated}/>
             </div>
         </div>
