237bc6f8b384ff923d9abb40ef266ca7ef7dba52
Legal-Brian <legal.b.n@outlook.com>
2022-12-16 17:48:20 -0300

    getTypes & filterPokemonsByType added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,28 @@
 import axios from "axios";
 
 export const getPokemons = () => {
-    return async function(dispatch){
+    return async (dispatch) =>{
         const json = await axios.get("http://localhost:3001/pokemons");
         return dispatch({
             type: "GET_POKEMONS",
             payload: json.data
         })
+    }
+};
+
+export const getTypes = () => {
+    return async (dispatch) =>{
+        const json = await axios.get("http://localhost:3001/types");
+        return dispatch({
+            type: "GET_TYPES",
+            payload: json.data
+        })
+    }
+};
+
+export const filterPokemonsByType = (payload) =>{
+    return {
+        type: "FILTER_BY_TYPE",
+        payload,
     }
 }
