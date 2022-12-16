aa80f0dd262ccf48c32ea837e5bda3bbdad425d8
Legal-Brian <legal.b.n@outlook.com>
2022-12-16 15:04:44 -0300

    classname added

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import "./Card.css"
+
 
 const Card = ({name, image, types}) => {
     return(
-        <div>
-            <h3>{name}</h3>
-            <img src={image} />
-            {types.length === 2 ? (
-                <div className="types">
-                    <h5 className="cardType">{types[0]}</h5>
-                    <h5 className="cardType">{types[1]}</h5>
+        <div className="container">
+            <figure className="card">
+                <div>
+                    <h3 className="cardName">{name}</h3>
                 </div>
-            ) : (
-                <div className="types">
-                    <h5 className="cardType">{types[0]}</h5>
+                <div className="cardImageContainer">
+                    <img src={image} />
                 </div>
-            )}
+                {types.length === 2 ? (
+                    <div className="types">
+                        <h5 className="cardType">{types[0]}</h5>
+                        <h5 className="cardType">{types[1]}</h5>
+                    </div>
+                ) : (
+                    <div className="types">
+                        <h5 className="cardType">{types[0]}</h5>
+                    </div>
+                )}
+            </figure>
         </div>
     )
 }
