860b1416b27532b7d0be1ff946551694c62bc997
Legal-Brian <legal.b.n@outlook.com>
2022-12-21 17:39:10 -0300

    styles updated

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import "./Card.css"
+import style from "./Card.module.css";
 
 
 const Card = ({name, image, types}) => {
     return(
-        <div className="container">
-            <figure className="card">
+        <div className={style.container}>
+            <figure className={style.card}>
                 <div>
-                    <h3 className="cardName">{name}</h3>
+                    <h3 className={style.cardName}>{name[0].toUpperCase() + name.slice(1)}</h3>
                 </div>
-                <div className="cardImageContainer">
-                    <img src={image} />
+                <div className={style.cardImageContainer}>
+                    <img className={style.pokemonImg} src={image} />
                 </div>
                 {types.length === 2 ? (
-                    <div className="types">
-                        <h5 className="cardType">{types[0]}</h5>
-                        <h5 className="cardType">{types[1]}</h5>
+                    <div className={style.types}>
+                        <h5 className={style.cardType}>{types[0]}</h5>
+                        <h5 className={style.cardType}>{types[1]}</h5>
                     </div>
                 ) : (
-                    <div className="types">
-                        <h5 className="cardType">{types[0]}</h5>
+                    <div className={style.types}>
+                        <h5 className={style.cardType}>{types[0]}</h5>
                     </div>
                 )}
             </figure>
